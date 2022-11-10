2bdb9538a05fc58c1df55254d1603f30804417ca
Mikhail_A <artemev1986@yandex.ru>
2022-11-10 18:12:08 +0500

    fix: add SQL exception

--- ewm/src/main/java/ru/yandex/practicum/ewm/controller/ErrorHandler.java
+++ ewm/src/main/java/ru/yandex/practicum/ewm/controller/ErrorHandler.java
@@ -11,6 +11,7 @@ import ru.yandex.practicum.ewm.exception.ForbiddenException;
 import ru.yandex.practicum.ewm.exception.ValidationException;
 
 import javax.persistence.EntityNotFoundException;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -58,4 +59,17 @@ public class ErrorHandler {
       log.warn(String.valueOf(e));
       return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
   }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleSqlException(final SQLException e) {
+        Arrays.stream(e.getStackTrace())
+                .map(StackTraceElement::toString)
+                .forEach(apiError.getErrors()::add);
+        apiError.setMessage(e.getMessage());
+        apiError.setReason("SQL exception");
+        apiError.setStatus("INTERNAL_SERVER_ERROR");
+        apiError.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        log.warn(String.valueOf(e));
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
